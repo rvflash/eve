0a03d9d2aeb1c8e69c9a7465e58db6f4effdf083
hgouchet <herve.gouchet@twenga.com>
2017-10-03 21:11:58 +0200

    Uses the latest version of elapsed package

--- vendor/github.com/rvflash/elapsed/time_test.go
+++ vendor/github.com/rvflash/elapsed/time_test.go
@@ -40,5 +40,11 @@ func TestTime(t *testing.T) {
 func ExampleTime() {
 	t := time.Now().Add(-time.Hour)
 	fmt.Println(elapsed.Time(t))
+	t = time.Now().Add(-time.Hour * 24 * 3)
+	fmt.Println(elapsed.Time(t))
+	t, _ = time.Parse("2006-02-01", "2049-08-19")
+	fmt.Println(elapsed.Time(t))
 	// Output: 1 hour ago
+	// 3 days ago
+	// not yet
 }
